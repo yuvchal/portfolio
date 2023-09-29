4e37621199813692991bdb0e737fdfa46fad9fc4
yuvchal <chahal.s.yuvraj@gmail.com>
2023-09-29 16:54:00 -0500

    Added first attempt at a prototype for the sign-in button. Not 100% working, but getting there

--- car-site/src/App.js
+++ car-site/src/App.js
@@ -1,7 +1,29 @@
 import logo from "./logo.svg";
 import "./App.css";
+import * as Msal from "msal";
 
-function App() {
+function App(props) {
+  const msalConfig = {
+    auth: {
+      clientId: "fd1fb936-7098-46ff-bd53-b62b405910ee",
+      authority:
+        "https://login.microsoftonline.com/44467e6f-462c-4ea2-823f-7800de5434e3",
+    },
+    cache: { cacheLocation: "localStorage" },
+  };
+  const msalInstance = new Msal.UserAgentApplication(msalConfig);
+  const signIn = async () => {
+    try {
+      // Use MSAL to initiate the sign-in process
+      const response = await msalInstance.loginPopup();
+
+      // Handle the sign-in response
+      console.log("User signed in:", response.account);
+      // You can access user information in response.account
+    } catch (error) {
+      console.error("Sign-in error:", error);
+    }
+  };
   return (
     <div className="App">
       <header className="App-header">
@@ -11,13 +33,14 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://youtube.com"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
         </a>
       </header>
+      <button onClick={signIn}>{}ayotest123</button>
     </div>
   );
 }
